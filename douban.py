a0f3cdf9fcc2eb939cd73c58e76ddc7a90243903
fanweiwei <funwewhere@foxmail.com>
2018-05-08 01:24:44 +0800

    获取歌曲链接

--- douban.py
+++ douban.py
@@ -1,36 +1,16 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-import urllib,ssl
+import urllib,json
 import urllib2
 import cookielib
 import gzip, StringIO
 
-def req(r, opener):
-    url = r.get_full_url()
-    print '\n'+url
-    response = opener.open(r)
-    responseInfo =  response.info()
-    data = response.read()
-    print responseInfo
-    if 'Content-Encoding' in responseInfo and 'gzip' == responseInfo['Content-Encoding']:
-        data = StringIO.StringIO(data)
-        gz = gzip.GzipFile(fileobj=data)
-        data = gz.read()
-        gz.close()
-
-    print '\n'
-    for ck in cj:
-        print ck.name + ':' + ck.value
-    print '\n'
-
-    print data
-    print url+'\n'
-
-# 登录的主页面
-hosturl = 'https://douban.fm'
-loginurl = 'https://accounts.douban.com/j/popup/login/basic'
-songsUrl = 'https://douban.fm/j/v2/redheart/basic'
-cookieUrl = 'https://douban.fm/j/check_loggedin?san=1'
+hostUrl = 'https://douban.fm'
+loginUrl = 'https://accounts.douban.com/j/popup/login/basic'
+getSongsInfoUrl = 'https://douban.fm/j/v2/redheart/basic'
+getCookieUrl = 'https://douban.fm/j/check_loggedin?san=1'
+getCookieUrl2 = 'https://www.douban.com/service/account/check_with_js?return_to=https%3A%2F%2Fdouban.fm%2Fj%2Fcheck_loggedin%3Fsan%3D1&sig=fef21efae8&r=0.2146490997849526&callback=_login_check_callback'
+getSongsLinkUrl = 'https://douban.fm/j/v2/redheart/songs'
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36',
@@ -49,37 +29,60 @@ headers2 = {
     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8'
 }
 
-postData = {
+loginPostData = {
     'source': 'fm',
     'referer': 'https://douban.fm/',
     'ck': None,
-    'name': 'username',
-    'password': 'pwd',
+    'name': 'funwewhere@foxmail.com',
+    'password': '478505231dou',
     'captcha_solution': None,
     'captcha_id': None,
     'remember': 'on'
 }
-postData = urllib.urlencode(postData)
 
-cj = cookielib.CookieJar()
-# cj.set_policy(policy)
-cookie_support = urllib2.HTTPCookieProcessor(cj)
-opener = urllib2.build_opener(cookie_support)
-urllib2.install_opener(opener)
+def sendRequest(request):
+    response = urllib2.urlopen(request)
+    responseInfo =  response.info()
+    result = response.read()
+    if 'Content-Encoding' in responseInfo and 'gzip' == responseInfo['Content-Encoding']:
+        result = StringIO.StringIO(result)
+        gz = gzip.GzipFile(fileobj=result)
+        result = gz.read()
+        gz.close()
+    return result
 
-req1 = urllib2.Request(hosturl)
-req2 = urllib2.Request(loginurl, postData, headers)
-req3 = urllib2.Request(cookieUrl, headers=headers)
-req4 = urllib2.Request('https://www.douban.com/service/account/check_with_js?return_to=https%3A%2F%2Fdouban.fm%2Fj%2Fcheck_loggedin%3Fsan%3D1&sig=fef21efae8&r=0.2146490997849526&callback=_login_check_callback', headers=headers)
-req5 = urllib2.Request(cookieUrl, headers=headers)
-req6 = urllib2.Request(songsUrl, headers=headers2)
 
-try:
-    req(req1, opener)
-    req(req2, opener)
-    req(req3, opener)
-    req(req4, opener)
-    req(req5, opener)
-    req(req6, opener)
-except Exception,e:
-    print Exception,":",e
+if __name__ == '__main__':
+    def domain_match(path, request):
+        return True
+
+    cookie = cookielib.CookieJar()
+    cookie._policy.domain_return_ok = domain_match
+    cookie._policy.return_ok_domain = domain_match
+
+    cookieHandle = urllib2.HTTPCookieProcessor(cookie)
+    opener = urllib2.build_opener(cookieHandle)
+    urllib2.install_opener(opener)
+
+    # try:
+    sendRequest(urllib2.Request(hostUrl))
+    sendRequest(urllib2.Request(loginUrl, urllib.urlencode(loginPostData), headers))
+    sendRequest(urllib2.Request(getCookieUrl, headers=headers))
+    sendRequest(urllib2.Request(getCookieUrl2, headers=headers))
+    responseData = sendRequest(urllib2.Request(getSongsInfoUrl, headers=headers2))
+    data = json.loads(responseData)
+
+    songsInfos = [t['sid'] for t in data['songs']]
+    postData = {
+        'sids' : "|".join(songsInfos),
+        'kbps': '320',
+        'ck': [c.value for c in cookie if c.name == 'ck'][0]
+    }
+    responseData = sendRequest(urllib2.Request(getSongsLinkUrl, urllib.urlencode(postData), headers2))
+    data = json.loads(responseData)
+
+    for arr in data[0]:
+        print arr,data[0][arr]
+
+    for t in [{'title': t['title'], 'albumtitle':t['albumtitle'], 'album':t['album'], 'url': t['url']} for t in data]:
+        print t
